83ec622ec2a553270575aba93e0e51e0abcea276
Michal2232 <elgawi@g.jct.ac.il>
2023-05-09 23:16:04 +0300

    my project

---
+++ BL/BlImplementation/Bl.cs
@@ -0,0 +1,12 @@
+﻿using BlApi;
+
+namespace BlImplementation;
+
+internal class Bl : IBl
+{
+    public Bl() { }
+    public IOrder Order { get;} = new Order();
+    public IProduct Product { get; } = new Product();
+    public ICart Cart { get; } = new Cart();
+
+}
